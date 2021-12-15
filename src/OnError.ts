5f8c45afcfbabb824310573d99231d42480a4ff5
Václav Oborník <vaclav.obornik@gmail.com>
2021-12-15 22:46:03 +0100

    feat: added onInfo option for convenient logging

--- src/OnError.ts
+++ src/OnError.ts
@@ -1,1 +1,15 @@
 export type OnError = (error: Error) => void;
+
+export const defaultOnError: OnError = (error) => {
+    console.error(error);
+};
+
+export function secureOnError(onError: OnError): OnError {
+    return (error) => {
+        try {
+            onError(error);
+        } catch (onErrorFailure) {
+            // intentionally suppress
+        }
+    };
+}
